b898d8f7baa8ce3b4f8ce78e15bd890d8b4aa118
fry <fry.land@hotmail.fr>
2022-12-30 07:04:50 +0100

    media queries header + main + about

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,14 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from '../styles/Header.module.css';
+import { HiOutlineMenuAlt1 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
+
   return (
     <div className={styles.headerContainer}>
       <header className={styles.header}>
@@ -14,8 +19,8 @@ export default function Header() {
             </h1>
           </Link>
         </div>
-        <div>
-          <ul className={styles.navContainer}>
+        <div className={styles.navContainer}>
+          <ul className={styles.navBar}>
             <li>
               <Link
                 href='/'
@@ -60,6 +65,69 @@ export default function Header() {
             </li>
           </ul>
         </div>
+        {!isOpen ? (
+          <button
+            className={styles.menuIconButton}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <HiOutlineMenuAlt1 className={styles.menuIcon} />
+          </button>
+        ) : (
+          <div className={styles.menuOpen}>
+            <button className={styles.closeIconButton}>
+              <AiOutlineClose
+                className={styles.closeIcon}
+                onClick={() => setIsOpen(!isOpen)}
+              />
+            </button>
+            <ul className={styles.navBarMenu}>
+              <li>
+                <Link
+                  href='/'
+                  className={router.pathname === '/' ? styles.active : null}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href='/about'
+                  className={
+                    router.pathname === '/about' ? styles.active : null
+                  }
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href='/work'
+                  className={router.pathname === '/work' ? styles.active : null}
+                >
+                  Work
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href='/blog'
+                  className={router.pathname === '/blog' ? styles.active : null}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href='/contact'
+                  className={
+                    router.pathname === '/contact' ? styles.active : null
+                  }
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </header>
     </div>
   );
