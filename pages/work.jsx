23433057b463f93051d7898ba87215b82ee95863
fry <fry.land@hotmail.fr>
2023-03-11 07:21:29 +0100

    starting work page

--- pages/work.jsx
+++ pages/work.jsx
@@ -1,3 +1,13 @@
+import Head from 'next/head';
+
 export default function work() {
-  return <div>work</div>;
+  return (
+    <>
+      <Head>
+        <title>Work page</title>
+        <meta name='description' content='My work' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+    </>
+  );
 }
